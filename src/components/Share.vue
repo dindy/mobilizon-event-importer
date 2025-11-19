693c73392b720f39b60e91c63f93defd904fc688
Sylvain Saint-André <sylvain.saintandre@gmail.com>
2025-11-19 17:05:51 +0100

    Implements share target capacity

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -2,52 +2,47 @@
 import { onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
+import { isValidUrl } from '../utils/utils'
 
 const route = useRoute()
 const store = useStore()
+const router = useRouter()
 store.dispatch('setPageTitle', 'Partager l\'événement')
 
 const title = ref('')
 const text = ref('')
 const url = ref('')
-const path = ref('')
-const otitle = ref('')
-const otext = ref('')
-const ourl = ref('')
-const opath = ref('')
+const toImport = ref(null)
 
 onMounted(() => {
-    const parsedUrl = new URL(window.location);
-    otitle.value = parsedUrl.searchParams.get('title')
-    otext.value = parsedUrl.searchParams.get('text')
-    ourl.value = parsedUrl.searchParams.get('url')
-    opath.value = window.location
-
     title.value = route.query.title
     text.value = route.query.text
-    url.value = route.query.url 
-    path.value = route.fullPath
-
-    store.commit('setScrapperUrl', url)
+    url.value = route.query.url
+    
+    if (url.value && isValidUrl(url.value)) {
+        toImport.value = url.value
+    } else if (text.value && isValidUrl(text.value)) {
+        toImport.value = text.value
+    }
 })
+
+const getText = () => `Vous allez importer l'évènement à l'URL ${toImport.value}`
+
+const go = () => {
+    store.commit('setScrapperUrl', toImport.value)
+    router.push('/identity')
+}
 </script>
 
 <template>
-    <h1>Import</h1>
-    <h2>Vanilla JS</h2>
-    <p>Titre: {{ otitle }}</p>
-    <p>Text: {{ otext }}</p>
-    <p>URL: {{ ourl }}</p>
-    <p>Path: {{ opath }}</p>
-    <h2>Router</h2>
-    <p>Titre:{{ title }}</p>
-    <p>Text:{{ text }}</p>
-    <p>URL:{{ url }}</p>
-    <p>Path:{{ path }}</p>
+    <v-alert title="Partager l'évènement" variant="outlined" type="info" v-if="toImport" :text="getText()"></v-alert>
+    <v-alert title="Partage impossible"  type="warning" v-else text="Nous sommes désolés mais aucune URL valide n'a été détectée."></v-alert>
+    <v-btn
+        class="mt-5"
+        color="primary"
+        @click="go"
+    >Confirmer</v-btn>   
 </template>
 
 <style scoped>
-    p {
-        margin: 10px 0;
-    }
 </style>
