ed6713c5824456c9214aa81a37579410b305085a
Sylvain Saint-André <sylvain.saintandre@gmail.com>
2026-01-31 17:37:10 +0100

    [BUG] Import components from router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Callback from './components/Callback.vue'
+import NotFound from './components/NotFound.vue' 
+import Home from './components/Home.vue' 
+import EventScrapper from './components/EventScrapper.vue' 
+import GroupScrapper from './components/GroupScrapper.vue' 
+import Event from './components/Event.vue' 
+import Group from './components/Group.vue' 
+import Done from './components/Done.vue' 
+import SelectInstance from './components/SelectInstance.vue' 
+import Share from './components/Share.vue' 
+import Welcome from './components/Welcome.vue' 
+import RegisterFeed from './components/RegisterFeed.vue' 
+import Automations from './components/Automations.vue' 
+import AutomationHistory from './components/AutomationHistory.vue' 
 
 const routes = [
     { path: '/', component: Welcome },
